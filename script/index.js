e1dc90c83fefe05f3dfc455c07aebe8e483ef823
Titova Anastasia <meofseal@gmail.com>
2022-03-15 14:53:40 +0300

    ревью 3

--- script/index.js
+++ script/index.js
@@ -1,26 +1,26 @@
 // переменные
-const popup = document.querySelector(".popup"); //сам поп ап
-const openPopupButton = document.querySelector(".profile__button-edit"); // открыть поп ап
-const closePopupButton = document.querySelector(".popup__close-button"); //скрыть поп ап
-let userName = document.querySelector(".profile__name"); // имя в профиле
-let nameInput = document.querySelector(".popup__form_type_name"); // имя в форме
-let userProfession = document.querySelector(".profile__info"); // работа в профиле
-let jobInput = document.querySelector(".popup__form_type_job"); // работа в форме
-const formElement = document.querySelector(".popup__profile");
+const popup = document.querySelector('.popup'); //сам поп ап
+const openPopupButton = document.querySelector('.profile__button-edit'); // открыть поп ап
+const closePopupButton = document.querySelector('.popup__close-button'); //скрыть поп ап
+let userName = document.querySelector('.profile__name'); // имя в профиле
+let nameInput = document.querySelector('.popup__form_type_name'); // имя в форме
+let userProfession = document.querySelector('.profile__info'); // работа в профиле
+let jobInput = document.querySelector('.popup__form_type_job'); // работа в форме
+const formElement = document.querySelector('.popup__profile');
 
 // открытие поп апа
 function openPopup() {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
   nameInput.value = userName.textContent;
   jobInput.value = userProfession.textContent;
 }
-openPopupButton.addEventListener("click", openPopup);
+openPopupButton.addEventListener('click', openPopup);
 
 // закрытие поп апа
 function closePopup() {
-  popup.classList.remove("popup_opened");
+  popup.classList.remove('popup_opened');
 }
-closePopupButton.addEventListener("click", closePopup);
+closePopupButton.addEventListener('click', closePopup);
 
 //сохранение
 function formSubmitHandler(evt) {
@@ -29,4 +29,4 @@ function formSubmitHandler(evt) {
   userProfession.textContent = jobInput.value;
   closePopup();
 }
-formElement.addEventListener("submit", formSubmitHandler);
+formElement.addEventListener('submit', formSubmitHandler);
