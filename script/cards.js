843491cdccf1d44819325f8cf94eeda80540ee2e
Titova Anastasia <meofseal@gmail.com>
2022-06-09 15:43:27 +0300

    6

---
+++ script/cards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "уточка",
+    link: "https://images.unsplash.com/photo-1578102487209-9229fa2b1cfb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://images.unsplash.com/photo-1580597095981-8d21aa8cdfed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Иваново",
+    link: "https://images.unsplash.com/photo-1597693421414-ddb414ca62c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Камчатка",
+    link: "https://images.unsplash.com/photo-1642631203400-c15d096a7f85?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://images.unsplash.com/photo-1631018743789-dcf87cc25308?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  },
+  {
+    name: "Байкал",
+    link: "https://images.unsplash.com/photo-1557704086-3e2f5969687b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+];
